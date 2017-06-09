5070e7fb040623e565a37fbdb78d1d0b748761f3
Michael Odumosu <michaelodumosu57@gmail.com>
2017-06-09 13:04:51 -0400

    Undone work 6/7/17, got fibonacci recusive strategy working with debug now to pretty print

--- fibonacci_series.cpp
+++ fibonacci_series.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
 using namespace std;
 
-//recursive prototype using call by value
+//recursive prototype using call by reference
 
 int fibo_function(int * len);
+//to debug the program
+static int debug;
+static int fib_seq_len;
+static int first = 0;
+static int second =1;
+static int next;
+int main(void) {
+	
+	//query
+	cout << "debug?\n";
+	cin >> debug;
+	cout << "Please enter the amonut of numbers you want to see in the fibonacci sequence\n";
+	cin >> fib_seq_len;
+	cout << "So this is the length that you want?   "  << fib_seq_len << endl;
+	//function call
+	fibo_function(&fib_seq_len);
+	cout << endl;
+	return 0;
 
-int main() {
-int fib_seq_len;
-cout << "Please enter the amonut of numbers you want to see in the fibonacci sequence\n";
-cin >> fib_seq_len;
-cout << "So this is the length that you want?   "  << fib_seq_len << endl;
-return 0;
+}	
 
-}
-
-//recursive function using call by value
+//recursive function using call by reference
 int fibo_function(int * len) {
-	int first = 0;
-	int second = 1;
+	// the count would refer to the len in main and is shared that way
 	cout << first << second;
-	int next;
-	if (len == 0) {
+	if (*len == 0) {
 		return 0;
 	}
 	else {
 		next = first + second;
-		cout << next;
+                if(debug == 0) {
+                        cout << "the next number in the sequence" << next << endl;
+                }
 		first = second;
 		second = next;	
-		len --;
+		*len -= 1;
+		if(debug == 0) {
+			cout << "the current length" << *len << endl;	
+		}
+		fibo_function(len);
 	}
-	
-}
+		
+}	
