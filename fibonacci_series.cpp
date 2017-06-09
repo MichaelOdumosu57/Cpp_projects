106f8918a3fbc3e4e97945b29ad34bd9f24d7e58
Michael Odumosu <michaelodumosu57@gmail.com>
2017-06-09 12:13:23 -0400

    Undone work 6/9/17, the program was able to retreive the desired number as the cutoff length for the fibonacci sequence

---
+++ fibonacci_series.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int fib_seq_len;
+
+cout << "Please enter the amonut of numbers you want to see in the fibonacci sequence\n";
+cin >> fib_seq_len;
+cout << "So this is the length that you want?   "  << fib_seq_len << endl;
+
+return 0;
+
+
+
+
+}
