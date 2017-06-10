f5316243df0f30e6988c2ea546d41bd6f527a33d
Michael Odumosu <michaelodumosu57@gmail.com>
2017-06-10 10:55:39 -0400

    Undone work 6/10/17 wrote switch statement to turn number into words

--- number_to_character.cpp
+++ number_to_character.cpp
@@ -14,10 +14,43 @@ int main (void) {
 	if (strcmp(debug,"yes") == 0){
 		cout << "this is your number " << word_number << endl;
 	}
-	cout << word_number/100 << endl;
+	converter(&word_number);
 	return 0;
 
 }
 void converter(int * num) {
-	
+	int digit = *num/100;
+	switch(digit) {
+            case 0:
+                cout << "zero ";
+                break;
+            case 1:
+                cout << "one ";
+                break;
+            case 2:
+                cout << "two ";
+                break;
+            case 3:
+                cout << "three ";
+                break;
+            case 4:
+                cout << "four ";
+                break;
+            case 5:
+                cout << "five ";
+                break;
+            case 6:
+                cout << "six";
+                break;
+            case 7:
+                cout << "seven ";
+                break;
+            case 8:
+                cout << "eight ";
+                break;
+            case 9:
+                cout << "nine ";
+                break;
+
+	}
 }
