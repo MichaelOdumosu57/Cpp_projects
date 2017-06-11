b0242395b8ba7a5b9f0bc8708ca832d5753b38d3
Michael Odumosu <michaelodumosu57@gmail.com>
2017-06-11 00:01:45 -0400

    Undone work 6/11/17 got alphabet program to pretty pretty, starting to form the triangle

--- alphabet_triangle.cpp
+++ alphabet_triangle.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 int main() {
         char ch = 65;
+	int count = 0;
+	while(count != 26) {
+		cout << " ";
+		count++;
+	}
 	while(ch != 91) {
 		std::cout << char(ch) << '\n';
                 ch += 1;
