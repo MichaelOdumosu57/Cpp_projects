631ece4a5d941f5d0ce20a8a33fea80b229986be
Michael Odumosu <michaelodumosu57@gmail.com>
2017-06-11 00:07:47 -0400

    Undone work 6/11/17 program prints first part of pyrmaid

--- alphabet_triangle.cpp
+++ alphabet_triangle.cpp
@@ -2,14 +2,18 @@
 #include <cstring>
 using namespace std;
 int main() {
-        char ch = 65;
-	int count = 0;
-	while(count != 26) {
-		cout << " ";
-		count++;
-	}
-	while(ch != 91) {
-		std::cout << char(ch) << '\n';
+        char ch = 65; //represents A from ascii, used so we wont use extra space making an array
+	int count = 0; // for spaces to build the pyramid
+	int desired = 26; // amount of spaces needed
+
+	while(ch != 91) { //91 = Z = end of alphabet
+        	while(count != desired) {
+                	cout << " ";          //making spaces
+                	count++;
+        	}
+		count = 0;
+		desired -- ;
+		std::cout << char(ch) << '\n'; //code that converts int to a string
                 ch += 1;
 	}
 }
