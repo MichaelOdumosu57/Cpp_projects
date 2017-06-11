eb7f63f52a233ed19c36655c0ad1aa894141e37d
Michael Odumosu <michaelodumosu57@gmail.com>
2017-06-11 00:18:22 -0400

    Undone work 6/11/17 program completely prints alphabet pyrmaid

--- alphabet_triangle.cpp
+++ alphabet_triangle.cpp
@@ -3,17 +3,27 @@
 using namespace std;
 int main() {
         char ch = 65; //represents A from ascii, used so we wont use extra space making an array
+	char helper = 65; // help the pyramid
 	int count = 0; // for spaces to build the pyramid
 	int desired = 26; // amount of spaces needed
 
-	while(ch != 91) { //91 = Z = end of alphabet
+	while(ch != 91) { //92 = Z = end of alphabet
         	while(count != desired) {
                 	cout << " ";          //making spaces
                 	count++;
         	}
 		count = 0;
 		desired -- ;
-		std::cout << char(ch) << '\n'; //code that converts int to a string
+		while(helper != ch) {
+			std::cout << char(helper); //code that converts int to a string
+			helper++;
+		}
+                while(helper >= 65) {
+                        std::cout << char(helper); //code that converts int to a string
+                        helper--;
+                }
+		cout << endl;
+		helper = 65; // so it wont overreach to @ which = 64
                 ch += 1;
 	}
 }
